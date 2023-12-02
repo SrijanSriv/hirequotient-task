a1ad8b52aa9e77e86bd95c1b0d37b1b56534d7bd
SrijanSriv <srijansrivastav01@gmail.com>
2023-12-02 10:31:22 +0530

    add: styling

---
+++ src/pages/api/data.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-// pages/index.tsx
 import { useState, useEffect } from 'react';
 
 const pageSize = 10;
 
 interface User {
-  originalData: User;
   id: string;
   name: string;
   email: string;
   role: string;
   isEditing: boolean;
+  originalData?: Partial<User>;
 }
 
 export default function Home() {
   const [users, setUsers] = useState<User[]>([]);
+  const [originalData, setOriginalData] = useState<User[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedRows, setSelectedRows] = useState<string[]>([]);
   const [selectAll, setSelectAll] = useState(false);
@@ -21,9 +21,10 @@ export default function Home() {
   const [searchColumn, setSearchColumn] = useState('name');
 
   useEffect(() => {
-    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+    fetch('/api/data')
       .then((response) => response.json())
-      .then((data: User[]) => setUsers(data.map((user) => ({ ...user, isEditing: false }))))
+      .then((data: User[]) => {setUsers(data.map((user) => ({ ...user, isEditing: false })))
+      setOriginalData(data.map((user) => ({ ...user, isEditing: false })))})
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
@@ -38,20 +39,16 @@ export default function Home() {
       const columnValue = (user[searchColumn as keyof User] as string).toLowerCase();
       return columnValue.includes(searchTermLowerCase);
     });
-  
+
     setCurrentPage(1);
     setUsers(filteredUsers);
   };
-  
 
   const handleClearSearch = () => {
     setSearchTerm('');
     setSearchColumn('name');
     setCurrentPage(1);
-    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
-      .then((response) => response.json())
-      .then((data: User[]) => setUsers(data.map((user) => ({ ...user, isEditing: false }))))
-      .catch((error) => console.error('Error fetching data:', error));
+    setUsers([...originalData]);
   };
 
   const handlePageChange = (newPage: number) => {
@@ -89,24 +86,32 @@ export default function Home() {
     );
   };
 
-  const handleSave = () => {
-    const updatedUsers = users.map((user) => ({
-      ...user,
-      isEditing: false,
-    }));
-    setUsers(updatedUsers);
+  const handleSave = (userId: string) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId
+          ? {
+              ...user,
+              isEditing: false,
+            }
+          : user
+      )
+    );
   };
 
   const handleCancel = (userId: string) => {
     setUsers((prevUsers) =>
       prevUsers.map((user) =>
         user.id === userId && user.isEditing
-          ? { ...user, ...(user.originalData || user) }
+          ? {
+              ...user,
+              isEditing: false,
+              ...(user.originalData || user),
+            }
           : user
       )
     );
   };
-  
 
   const handleEditChange = (value: string, userId: string, property: string) => {
     setUsers((prevUsers) =>
@@ -125,8 +130,8 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <div className="relative mb-4">
+    <div className="container mx-auto p-8">
+      <div className="mb-4">
         <select
           onChange={(e) => setSearchColumn(e.target.value)}
           className="border border-gray-300 p-2 rounded mr-2"
@@ -138,142 +143,144 @@ export default function Home() {
         <input
           type="text"
           placeholder="Search..."
+          value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
           className="border border-gray-300 p-2 rounded w-64 mr-2"
         />
         <button
-          className="bg-blue-500 text-white px-2 py-1"
+          className="bg-violet-400 text-white px-4 py-2 mr-2 rounded hover:shadow-lg"
           onClick={handleSearch}
         >
           Search
         </button>
-        {searchTerm && (
           <button
-            className="bg-gray-400 text-white px-2 py-1"
+            className="bg-gray-200 px-4 py-2 rounded hover:shadow-lg"
             onClick={handleClearSearch}
           >
             Clear Search
           </button>
-        )}
       </div>
-      <table className="table-auto w-full border-collapse border border-gray-800 mt-4">
-        <thead>
-          <tr>
-            <th className="border border-gray-800 p-2">
+      <table className="table-auto w-full border-collapse border border-gray-300 shadow-lg">
+      <thead>
+        <tr className="bg-violet-400 text-white">
+          <th className="border border-black-300 p-2">
+            <input
+              type="checkbox"
+              checked={selectAll}
+              onChange={() => setSelectAll(!selectAll)}
+            />
+          </th>
+          <th className="border border-gray-300 p-2">ID</th>
+          <th className="border border-gray-300 p-2">Name</th>
+          <th className="border border-gray-300 p-2">Email</th>
+          <th className="border border-gray-300 p-2">Role</th>
+          <th className="border border-gray-300 p-2">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {visibleUsers.map((user) => (
+          <tr key={user.id} className="text-center">
+            <td className="border border-gray-300 p-2">
               <input
                 type="checkbox"
-                checked={selectAll}
-                onChange={() => setSelectAll(!selectAll)}
+                checked={selectedRows.includes(user.id)}
+                onChange={() => handleSelectRow(user.id)}
               />
-            </th>
-            <th className="border border-gray-800 p-2">ID</th>
-            <th className="border border-gray-800 p-2">Name</th>
-            <th className="border border-gray-800 p-2">Email</th>
-            <th className="border border-gray-800 p-2">Role</th>
-            <th className="border border-gray-800 p-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {visibleUsers.map((user) => (
-            <tr key={user.id} className="text-center">
-              <td className="border border-gray-800 p-2">
+            </td>
+            <td className="border border-gray-300 p-2">{user.id}</td>
+            <td className="border border-gray-300 p-2">
+              {user.isEditing ? (
                 <input
-                  type="checkbox"
-                  checked={selectedRows.includes(user.id)}
-                  onChange={() => handleSelectRow(user.id)}
+                  type="text"
+                  value={user.name}
+                  onChange={(e) => handleEditChange(e.target.value, user.id, 'name')}
+                  className="border border-gray-300 p-1 rounded"
                 />
-              </td>
-              <td className="border border-gray-800 p-2">{user.id}</td>
-              <td className="border border-gray-800 p-2">
-                {user.isEditing ? (
-                  <input
-                    type="text"
-                    value={user.name}
-                    onChange={(e) =>
-                      handleEditChange(e.target.value, user.id, 'name')
-                    }
-                    className="border border-gray-300 p-1 rounded"
-                  />
-                ) : (
-                  <div onClick={() => handleEdit(user.id)}>{user.name}</div>
-                )}
-              </td>
-              <td className="border border-gray-800 p-2">
-                {user.isEditing ? (
-                  <input
-                    type="text"
-                    value={user.email}
-                    onChange={(e) =>
-                      handleEditChange(e.target.value, user.id, 'email')
-                    }
-                    className="border border-gray-300 p-1 rounded"
-                  />
-                ) : (
-                  <div onClick={() => handleEdit(user.id)}>{user.email}</div>
-                )}
-              </td>
-              <td className="border border-gray-800 p-2">
-                {user.isEditing ? (
-                  <input
-                    type="text"
-                    value={user.role}
-                    onChange={(e) =>
-                      handleEditChange(e.target.value, user.id, 'role')
-                    }
-                    className="border border-gray-300 p-1 rounded"
-                  />
-                ) : (
-                  <div onClick={() => handleEdit(user.id)}>{user.role}</div>
-                )}
-              </td>
-              <td className="border border-gray-800 p-2">
-                {user.isEditing ? (
-                  <>
-                    <button
-                      className="bg-green-500 text-white px-2 py-1 mr-2 rounded"
-                      onClick={() => handleSave()}
-                    >
-                      Save
-                    </button>
-                    <button
-                      className="bg-gray-400 text-white px-2 py-1 rounded"
-                      onClick={() => handleCancel(user.id)}
-                    >
-                      Cancel
-                    </button>
-                  </>
-                ) : (
+              ) : (
+                <div onClick={() => handleEdit(user.id)} className="cursor-pointer">
+                  {user.name}
+                </div>
+              )}
+            </td>
+            <td className="border border-gray-300 p-2">
+              {user.isEditing ? (
+                <input
+                  type="text"
+                  value={user.email}
+                  onChange={(e) => handleEditChange(e.target.value, user.id, 'email')}
+                  className="border border-gray-300 p-1 rounded"
+                />
+              ) : (
+                <div onClick={() => handleEdit(user.id)} className="cursor-pointer">
+                  {user.email}
+                </div>
+              )}
+            </td>
+            <td className="border border-gray-300 p-2">
+              {user.isEditing ? (
+                <input
+                  type="text"
+                  value={user.role}
+                  onChange={(e) => handleEditChange(e.target.value, user.id, 'role')}
+                  className="border border-gray-300 p-1 rounded"
+                />
+              ) : (
+                <div onClick={() => handleEdit(user.id)} className="cursor-pointer">
+                  {user.role}
+                </div>
+              )}
+            </td>
+            <td className="border border-gray-300 p-2">
+              {user.isEditing ? (
+                <>
                   <button
-                    className="bg-blue-500 text-white px-2 py-1"
+                    className="bg-green-500 text-white px-2 py-1 mr-2 rounded hover:bg-green-700"
+                    onClick={() => handleSave(user.id)}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="bg-gray-400 text-white px-2 py-1 rounded hover:bg-gray-600"
+                    onClick={() => handleCancel(user.id)}
+                  >
+                    Cancel
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    className="bg-violet-400 text-white px-2 py-1 mr-2 rounded hover:bg-violet-600"
                     onClick={() => handleEdit(user.id)}
                   >
-                    Edit
+                    ✏️
                   </button>
-                )}
-                <button
-                  className="bg-red-500 text-white px-2 py-1 ml-2 rounded"
-                  onClick={() => handleDelete(user.id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  <button
+                    className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    🗑️
+                  </button>
+                </>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
       <div className="mt-4 flex justify-between items-center">
         <div>
           <button
-            className="bg-blue-500 text-white px-2 py-1 mr-2"
+            className="bg-slate-200 px-2 py-1 mr-2 rounded hover:bg-slate-400"
             onClick={() => handlePageChange(currentPage - 1)}
             disabled={currentPage === 1}
           >
-            Previous Page
+            ← Previous Page
           </button>
           {Array.from({ length: totalPages }).map((_, index) => (
             <button
               key={index}
-              className={`bg-blue-500 text-white px-2 py-1 mr-2 ${
+              className={`bg-slate-200 px-2 py-1 mr-2 rounded hover:bg-slate-400 ${
                 currentPage === index + 1 ? 'bg-gray-800' : ''
               }`}
               onClick={() => handlePageChange(index + 1)}
@@ -282,22 +289,22 @@ export default function Home() {
             </button>
           ))}
           <button
-            className="bg-blue-500 text-white px-2 py-1"
+            className="bg-slate-200 px-2 py-1 rounded hover:bg-slate-400"
             onClick={() => handlePageChange(currentPage + 1)}
             disabled={currentPage === totalPages}
           >
-            Next Page
+            Next Page →
           </button>
         </div>
         <div>
-          <span>{`Page ${currentPage} of ${totalPages}`}</span>
+          <span className="text-lg">{`Page ${currentPage} of ${totalPages}`}</span>
         </div>
       </div>
       <button
-        className="bg-red-500 text-white px-2 py-1 mt-4"
+        className="bg-red-500 text-white px-2 py-1 mt-4 rounded hover:bg-red-700"
         onClick={() => handleDeleteSelected()}
       >
-        Delete Selected
+        🗑️ Delete Selected
       </button>
     </div>
   );
